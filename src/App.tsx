ef5cc60c2b4ac1b31d6e9edf0b39959f93516c62
Nick <nick22783@gmail.com>
2024-03-28 17:53:51 +0300

    4. Query Error State

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,20 @@ import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import axios from "axios";
 
 function App() {
-  const queryInfo = useQuery("pokemon", () =>
-    axios("https://pokeapi.co/api/v2/pokemon").then((res) => res.data.results),
-  );
+  const queryInfo = useQuery("pokemon", () => {
+    // if (true) {
+    //   throw new Error("Test error");
+    // }
+
+    return axios("https://pokeapi.co/api/v2/pokemon").then(
+      (res) => res.data.results,
+    );
+  });
 
   return queryInfo.isLoading ? (
     <div>"Loading..."</div>
+  ) : queryInfo.error instanceof Error ? (
+    <div>{queryInfo.error.message}</div>
   ) : (
     <div className="App">
       {queryInfo.data.map((result) => {
