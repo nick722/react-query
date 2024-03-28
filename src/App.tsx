ee4953c1c6ae8152cb0ff75a813269ce8d8ea571
Nick <nick22783@gmail.com>
2024-03-28 17:04:40 +0300

    2.Basic Queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import axios from "axios";
 
 function App() {
   const queryInfo = useQuery("pokemon", () =>
-    fetch("https://pokeapi.com/api/v2/pokemon").then((res) => res.json()),
+    axios("https://pokeapi.co/api/v2/pokemon").then((res) => res.data.results),
   );
 
   console.log("queryInfo", queryInfo);
 
-  return <div className="App">hi</div>;
+  return (
+    <div className="App">
+      {queryInfo.data?.map((result) => {
+        return <div key={result.name}>{result.name}</div>;
+      })}
+    </div>
+  );
 }
 
 export default App;
